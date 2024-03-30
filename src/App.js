c14418559e7c611e352f3f8505ac0184bdeb1e8b
Prem <premkhodke.99@gmail.com>
2024-03-30 18:23:44 +0530

    product cards

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/home';
 import Cart from './pages/cart';
+import Header from './components/header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Navbar</h1>
+      <Header />
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/cart" element={<Cart />} />
